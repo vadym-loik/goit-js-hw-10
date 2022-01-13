f78166362f652a68bb7978e089951017823bac0d
Vadym <loikfit@gmail.com>
2022-01-13 19:36:47 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function renderCountryList(countries) {
   const markup = countries
     .map(({ name, flags }) => {
       return `
-            <li class="country-list__item">
+            <li class="country-list__item list">
                 <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 35 height = 35px>
                 <h2 class="country-list__name">${name.official}</h2>
             </li>
@@ -53,7 +53,7 @@ function renderCountryInfo(countries) {
   const markup = countries
     .map(({ capital, population, languages }) => {
       return `
-          <ul class="country-info__list">
+          <ul class="country-info__list list">
               <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
               <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
               <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(
